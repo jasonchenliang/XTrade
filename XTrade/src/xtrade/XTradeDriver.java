ab02f834c0ad31cf03f181e49c4632864e93591e
jasonchenliang <jasonchenliang@gmail.com>
2013-05-29 03:21:13 -0700

    Added new test methods.

--- XTrade/src/xtrade/XTradeDriver.java
+++ XTrade/src/xtrade/XTradeDriver.java
@@ -18,28 +18,34 @@ public class XTradeDriver {
         newXTrade.fetchStock();
         
         //display all stocks' price
-        newXTrade.displayStock();
+        newXTrade.query();
         
         //update the stock price of "google"
         newXTrade.updateStock("google",2000.0);
-        
-        //display the stock price of "google"
-        newXTrade.displayStock("google");
-        
-        
-        
-   
+
         //initialize all user data
         newXTrade.fetchUser();
         
         //display all user data
         newXTrade.displayUser();
-
+        
         //update user "jason" with 2000 credit
         newXTrade.updateUser("jason",2000.0);
         
-        //display user "jason"
-        newXTrade.displayUser("jason");
-
+        //login as new user "mike", $1000 will be credited
+        newXTrade.loginUser("mike");        
+        
+        
+        //initialize all transactions
+        newXTrade.fetchTransaction();
+        
+        //display all transactions
+        newXTrade.displayTransaction();
+        
+        newXTrade.sell("jason","google",1);
+        
+        //display all transactions - should be google*0
+        newXTrade.displayTransaction();       
+        
     }
 }
