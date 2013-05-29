34c287ba59f4932b7dcc3b0a8e1c7e51d703c4b6
jasonchenliang <jasonchenliang@gmail.com>
2013-05-29 03:20:45 -0700

    Added sell/buy operations, check methods and more...

--- XTrade/src/xtrade/XTrade.java
+++ XTrade/src/xtrade/XTrade.java
@@ -14,7 +14,9 @@ public class XTrade {
     private static Date date=new Date();;
     private static DateFormat dateFormat=new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
     private static ArrayList<Stock> stockList = new ArrayList<>();
-    private static ArrayList<User> userList= new ArrayList<>();        
+    private static ArrayList<User> userList= new ArrayList<>();
+    private static ArrayList<Transaction> transactionList= new ArrayList<>();
+    
   
     /*
      * Constructor of XTrade 
@@ -26,7 +28,7 @@ public class XTrade {
     /*
      * Return true if the stock exists.
      */
-    public boolean isStockExisted(String stockName)
+    private boolean isStockExisted(String stockName)
     {
         for(int i=0;i<stockList.size();i++)
        {
@@ -42,7 +44,7 @@ public class XTrade {
     /*
      * Return the stock object given the stock name
      */
-    public Stock getStock(String stockName)
+    private Stock getStock(String stockName)
     {
          for(int i=0;i<stockList.size();i++)
         {
@@ -51,13 +53,13 @@ public class XTrade {
                return stockList.get(i);
            }
         }
-         return stockList.get(0);
+         return stockList.get(0);//should never execute this line.
     }
     
     /*
      * Display all Stocks' price
      */
-    public void displayStock()
+    public void query()
     {
         for(int i=0;i<stockList.size();i++)
         {
@@ -69,7 +71,7 @@ public class XTrade {
      *
      * @param stockName the name of the stock to query its price
      */
-    public void displayStock(String stockName)
+    public void query(String stockName)
     {
        if(isStockExisted(stockName))
        {
@@ -106,21 +108,13 @@ public class XTrade {
      */
     public void updateStock(String stockName, double price)
     {
-        if(isStockExisted(stockName))
-        {
             getStock(stockName).setPrice(price);
             System.out.println("Stock price of ["+getStock(stockName).getName()+ "] is updated at "+dateFormat.format(date));
-        }
-        else
-        {
-            displayStock(stockName);//stock not found.
-        }
     }
     
     
      /*
-     * Pull the data from the file.
-     * Should be implemented using the real data from the file.
+     * Should Pull the data from the file.
      * HARD CODED some data thou.
      */
     public void fetchUser()
@@ -134,7 +128,7 @@ public class XTrade {
         userList.add(newUser2);
         userList.add(newUser3);
                
-        System.out.println("All user cash balance are updated at "+dateFormat.format(date));
+        System.out.println("All users' cash balance are updated at "+dateFormat.format(date));
 
     }
     
@@ -170,7 +164,7 @@ public class XTrade {
     /*
      * Return true if user exists.
      */
-    public boolean isUserExisted(String userName)
+    private boolean isUserExisted(String userName)
     {
         for(int i=0;i<userList.size();i++)
         {
@@ -186,7 +180,7 @@ public class XTrade {
     /*
      * Return the User object given the user name.
      */
-    public User getUser(String userName)
+    private User getUser(String userName)
     {
         for(int i=0;i<userList.size();i++)
         {
@@ -196,7 +190,7 @@ public class XTrade {
            }
         }
         
-        return userList.get(0);
+        return userList.get(0);//should never execute this line.
     }
     
     
@@ -205,26 +199,168 @@ public class XTrade {
      */
     public void updateUser(String userName,double credit)
     {
-        if(isUserExisted(userName))
+        getUser(userName).setCashBalance(getUser(userName).getCashBalance()+credit);
+        System.out.println("Cash balance of user ["+getUser(userName).getUserName()+ "] is updated at "+dateFormat.format(date));
+    }
+    
+    
+    /*
+     * 
+     */
+    public void loginUser(String userName)
+    {
+        System.out.println("Welcome to XTrade!");
+        
+        if(!isUserExisted(userName))
         {
-            if((getUser(userName).getCashBalance()+credit)>=0)
+           userList.add(new User(userName));
+        }
+          
+        displayUser(userName);
+    }
+      
+    
+     /*
+     * Should pull the data from the file.
+     * HARD CODED some data thou.
+     */
+    public void fetchTransaction()
+    {
+        Transaction newTransaction1 = new Transaction("Jason","google",1);
+        Transaction newTransaction2 = new Transaction("Jason","amazon",1);
+        Transaction newTransaction3 = new Transaction("Jason","tesla",1);
+        
+        transactionList.add(newTransaction1);
+        transactionList.add(newTransaction2);
+        transactionList.add(newTransaction3);
+        
+        System.out.println("All transaction records are updated at "+dateFormat.format(date));
+    }
+    
+    /*
+     * Display all transactions
+     */
+    public void displayTransaction()
+    {
+        for(int i=0;i<transactionList.size();i++)
+        {
+            System.out.println(transactionList.get(i).getUserName()+": "+transactionList.get(i).getStockName()+" * "+transactionList.get(i).getShares());
+        }
+        
+    }
+    
+    /* 
+     * @param userName the name of user to query
+     * Display transactions of the user
+     */
+    public void displayTransaction(String userName)
+    {
+        int count=0;
+        for(int i=0;i<transactionList.size();i++)
+        {
+            if(transactionList.get(i).getUserName().equalsIgnoreCase(userName))
             {
-                   getUser(userName).setCashBalance(getUser(userName).getCashBalance()+credit);
-                   System.out.println("Cash balance of user ["+getUser(userName).getUserName()+ "] is updated at "+dateFormat.format(date));
-            }
-            else
-            {
-                System.out.print("No sufficient fund for - ");
-                displayUser(userName);
+                count++;
+                System.out.println(transactionList.get(i).getUserName()+": "+transactionList.get(i).getStockName()+" * "+transactionList.get(i).getShares());
             }
         }
-        else
+        
+        if(count==0)
         {
-                displayUser(userName);//user not found
+            System.out.println("["+userName+"] has no transaction record.");
         }
     }
     
     
+    /* 
+     *Retrun ture if the user owns at least that many shares of stock
+     */
+    private boolean hasEnoughStock(String userName,String stockName,int shares)
+    {
+        for(int i=0;i<transactionList.size();i++)
+        {
+            if(transactionList.get(i).getUserName().equalsIgnoreCase(userName)&&transactionList.get(i).getStockName().equalsIgnoreCase(stockName)&&transactionList.get(i).getShares()>=shares)
+            {
+              return true;
+            }
+        }
+        return false;
+    }
+    
+     /* 
+     *Retrun ture if the user owns enough balance to buy that many of stocks
+     */
+    private boolean hasEnoughBalance(String userName,String stockName,int shares)
+    {
+        for(int i=0;i<transactionList.size();i++)
+        {
+            if(getStock(stockName).getPrice()*shares<=getUser(userName).getCashBalance())
+            {
+              return true;
+            }
+        }
+        return false;
+    }
+    
+    /*
+     * Update transaction record
+     */
+    public void updateTransaction(String userName,String stockName,int shares)
+    {
+        for(int i=0;i<transactionList.size();i++)
+        {
+            if(transactionList.get(i).getUserName().equalsIgnoreCase(userName)&&transactionList.get(i).getStockName().equalsIgnoreCase(stockName))
+            {
+                transactionList.get(i).setShares(transactionList.get(i).getShares()+shares);
+            }
+        }
+    }
     
     
+   public void buy(String userName,String stockName,int shares)
+   {
+       if(isUserExisted(userName)&&isStockExisted(stockName))
+       {
+           if(hasEnoughBalance(userName,stockName,shares))
+           {
+              updateUser(userName,-shares*getStock(stockName).getPrice());
+              updateTransaction(userName,stockName,shares);
+           }
+           else
+           {
+               System.out.println("No sufficient balance to buy: "+stockName+" * "+shares+" @ $"+getStock(stockName).getPrice());
+           }
+             
+       }
+       else
+       {
+           displayUser(userName);//user name is not found OR
+           query(stockName);//stock name is not found
+       }
+       
+   }
+   
+   
+   public void sell(String userName,String stockName,int shares)
+   {
+       if(isUserExisted(userName)&&isStockExisted(stockName))
+       {
+           if(hasEnoughStock(userName,stockName,shares))
+            {
+                updateUser(userName,shares*getStock(stockName).getPrice());
+                updateTransaction(userName,stockName,-shares);
+            }
+            else
+            {
+                System.out.println("No sufficient shares to sell: "+stockName+" * "+shares+" @ $"+getStock(stockName).getPrice());
+            }
+       }
+       else
+       {
+           displayUser(userName);//user name is not found OR
+           query(stockName);//stock name is not found
+       }
+     
+   }
+   
 }
