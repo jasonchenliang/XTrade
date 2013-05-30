ccebae3c8134b96db963a4144cae282eb8380382
jasonchenliang <jasonchenliang@gmail.com>
2013-05-30 16:54:38 -0700

    update

---
+++ XTrade/src/object/User.java
@@ -0,0 +1,47 @@
+package object;
+
+/**
+ *
+ * @author chenliang
+ */
+public class User {
+    private String userName;
+    private double cashBalance;
+
+    /*
+     * User constructor 
+     */
+    public  User(String newUserName)
+    {
+        this.setUserName(newUserName);
+        this.setCashBalance(1000.0);
+    }
+
+    /**
+     * @return the userName
+     */
+    public String getUserName() {
+        return userName;
+    }
+
+    /**
+     * @param userName the userName to set
+     */
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    /**
+     * @return the cashBalance
+     */
+    public double getCashBalance() {
+        return cashBalance;
+    }
+
+    /**
+     * @param cashBalance the cashBalance to set
+     */
+    public void setCashBalance(double cashBalance) {
+        this.cashBalance = cashBalance;
+    }
+}
