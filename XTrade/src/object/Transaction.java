c0ed60295c26c02716a2f68fb8ddf0187181c346
jasonchenliang <jasonchenliang@gmail.com>
2013-05-30 16:54:04 -0700

    update

---
+++ XTrade/src/object/Transaction.java
@@ -0,0 +1,67 @@
+package object;
+
+/**
+ *
+ * @author chenliang
+ */
+public class Transaction {
+    private String userName;
+    private String stockName;
+    private int shares;
+
+    /*
+     * Transaction constructor
+     */
+    public Transaction(String userName,String stockName,int shares)
+    {
+        setUserName(userName);
+        setStockName(stockName);
+        setShares(shares);
+      
+    }
+    
+    
+    /**
+     * @return the userName
+     */
+    public String getUserName() {
+        return userName;
+    }
+
+    /**
+     * @param userName the userName to set
+     */
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    /**
+     * @return the stockName
+     */
+    public String getStockName() {
+        return stockName;
+    }
+
+    /**
+     * @param stockName the stockName to set
+     */
+    public void setStockName(String stockName) {
+        this.stockName = stockName;
+    }
+
+    /**
+     * @return the shares
+     */
+    public int getShares() {
+        return shares;
+    }
+
+    /**
+     * @param shares the shares to set
+     */
+    public void setShares(int shares) {
+        this.shares = shares;
+    }
+    
+    
+}
