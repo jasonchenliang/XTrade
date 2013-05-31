02690b1382390918818dbf97a5e78ec73b206ac6
jasonchenliang <jasonchenliang@gmail.com>
2013-05-30 18:52:48 -0700

    client2

---
+++ XTrade/src/client/Client2.java
@@ -0,0 +1,24 @@
+package client;
+
+/**
+ *
+ * @author chenliang
+ */
+import java.rmi.registry.*;
+import xtrade.*;
+
+public class Client2 {
+    private static final String HOST = "localhost";
+    private static final int PORT = 1099;
+    private static Registry registry;
+
+    public static void main(String[] args) throws Exception {
+        registry = LocateRegistry.getRegistry(HOST, PORT);
+        
+        //new XTrade instance 
+        XTrade remoteXTrade = (XTrade) registry.lookup(XTrade.class.getSimpleName());
+        
+        System.out.println("client2 is conected.");
+
+    }
+}
