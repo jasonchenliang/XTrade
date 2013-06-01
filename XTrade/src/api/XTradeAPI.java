55e451a6b6216e93e4c21cf5b08c71aa0f28d7a3
jasonchenliang <jasonchenliang@gmail.com>
2013-05-31 22:27:04 -0700

    updated API

--- XTrade/src/api/XTradeAPI.java
+++ XTrade/src/api/XTradeAPI.java
@@ -11,12 +11,22 @@ import object.*;
 public interface XTradeAPI extends Remote{
 
     public String hello() throws RemoteException;
-                
-    public boolean isStockExisted(String stockName) throws RemoteException;
-
-    public ArrayList<Stock> query() throws RemoteException;
     
-    public Stock query(String symbol) throws RemoteException;
+    public String login(String userName) throws RemoteException;
+    
+    public String queryUser(String userName) throws RemoteException;
+    
+    public String queryStock(String symbol) throws RemoteException;
+    
+    public String queryRecord(String userName,String symbol) throws RemoteException;
+    
+    //client 1 updates
+    public String update(String symbol, double price) throws RemoteException;
+    
+    //client 2 buys/sells
+    public String buy(String symbol,String userName,int shares) throws RemoteException;
+    
+    public String sell(String symbol,String userName,int shares) throws RemoteException;
     
     
 }
